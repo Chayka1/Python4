0e086ca5045806fb6737b5e4f6315835fb8582cd
Chayka1 <106668633+Chayka1@users.noreply.github.com>
2022-12-12 14:22:40 +0200

    1

--- task.py
+++ task.py
@@ -3,9 +3,35 @@ def task_1_1():
     name.write('Max')
     name.close()
 
-def task_2_2():
+
+def task_1_2():
     name = open('files/my_name.txt', 'r')
+
     for name_ in name:
         print(name_)
+
     name.close()
+
+
+def task_1_3():
+    nums = open('files/number_list.txt', 'w')
+
+    for nums_ in range(1, 101):
+        nums.write(str(nums_))
+        nums.write('\n')
+
+    nums.close()
+
+
+def task_1_4():
+    nums = open('files/number_list.txt', 'r')
+
+    for nums_ in nums:
+        nums_ = nums_.rstrip('\n')
+        print(nums_)
+
+    nums.close()
+
+
+def task_1_5():
 
