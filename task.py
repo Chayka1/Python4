14ac047b3e2248aaa14f5438ad214b3d491c6ae2
Chayka1 <106668633+Chayka1@users.noreply.github.com>
2022-12-12 14:10:25 +0200

    1

--- task.py
+++ task.py
@@ -3,3 +3,9 @@ def task_1_1():
     name.write('Max')
     name.close()
 
+def task_2_2():
+    name = open('files/my_name.txt', 'r')
+    for name_ in name:
+        print(name_)
+    name.close()
+
